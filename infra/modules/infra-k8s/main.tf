d238e38aae33d6e175592e6a610ddd5fc59c34e2
caio.coelho <caio.coelho@totvs.com.br>
2025-11-16 20:30:59 -0300

    feat: arquivos .tf VPC + K8s + RDS e pipelines

--- infra/modules/infra-k8s/main.tf
+++ infra/modules/infra-k8s/main.tf
@@ -3,6 +3,35 @@ resource "aws_iam_user" "principal_user" {
   name = var.user_name
 }
 
+resource "aws_iam_policy" "ssm_access" {
+  name        = "SSMParameterAccessForEKS"
+  description = "Permite acesso a parâmetros SSM para AMI otimizada do EKS"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ssm:DescribeParameters"
+        ]
+        Resource = "*"
+        Effect   = "Allow"
+      },
+      {
+        Action = [
+          "ssm:GetParameter"
+        ]
+        Resource = "arn:aws:ssm:*:*:parameter/aws/service/eks/optimized-ami/"
+        Effect   = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "ssm_access" {
+  user       = aws_iam_user.principal_user.name
+  policy_arn = aws_iam_policy.ssm_access.arn
+}
+
 resource "aws_iam_user_policy_attachment" "iam_readonly" {
   user       = aws_iam_user.principal_user.name
   policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
