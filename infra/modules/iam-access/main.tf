c239ed16568a73a699991692bde7559524123b72
caio.coelho <caio.coelho@totvs.com.br>
2025-11-16 21:22:59 -0300

    feat: arquivos .tf VPC + K8s + RDS e pipelines

--- infra/modules/iam-access/main.tf
+++ infra/modules/iam-access/main.tf
@@ -3,6 +3,9 @@ resource "aws_iam_user" "principal_user" {
   name = var.user_name
 }
 
+# Data source para obter o ID da conta AWS atual
+data "aws_caller_identity" "current" {}
+
 resource "aws_iam_policy" "ssm_access" {
   name        = "SSMParameterAccessForEKS"
   description = "Permite acesso a parâmetros SSM para AMI otimizada do EKS"
@@ -10,17 +13,18 @@ resource "aws_iam_policy" "ssm_access" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "Statement1"
+        Sid      = "AllowEKSAMIParameterAccess"
         Effect   = "Allow"
-        Action   = [
-          "ssm:GetParameter",
-          "ssm:GetParameters",
-          "ssm:GetParametersByPath"
-        ]
-        Resource = [
-          "arn:aws:ssm:us-east-1::parameter/aws/service/eks/optimized-ami/1.33/amazon-linux-2023/x86_64/standard/recommended/release_version",
-          "arn:aws:ssm:us-east-1:aws:parameter/aws/service/eks/optimized-ami/"
-        ]
+        Action   = "ssm:GetParameter"
+        # Usando o ID da conta dinâmico
+        Resource = "arn:aws:ssm:us-east-1:${data.aws_caller_identity.current.account_id}:parameter/aws/service/eks/optimized-ami/*"
+      },
+      {
+        Sid      = "AllowEKSAMIParameterAccessForSpecificVersion"
+        Effect   = "Allow"
+        Action   = "ssm:GetParameter"
+        # O ARN de parâmetro de serviço não precisa do ID da conta
+        Resource = "arn:aws:ssm:us-east-1::parameter/aws/service/eks/optimized-ami/1.33/amazon-linux-2023/x86_64/standard/recommended/release_version"
       }
     ]
   })
